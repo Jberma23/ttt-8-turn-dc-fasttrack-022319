7e3901e674f19d537dbaa4f58dc4597f5ba6cc4f
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-10 18:12:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,68 +1,37 @@
-#input_to_index
-def input_to_index(user_input)
-  user_input = user_input.to_i
-  if user_input < 0 || user_input > 10 
-  end  
-  return user_input - 1
+def display_board(board)
+  puts " #{board [0]} " + "|" + " #{board [1]} " + "|" + " #{board [2]} "
+  puts "-----------"
+  puts " #{board [3]} " + "|" + " #{board [4]} " + "|" + " #{board [5]} "
+  puts "-----------"
+  puts " #{board [6]} " + "|" + " #{board [7]} " + "|" + " #{board [8]} "
 end
 
+ def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  input_int = input_to_index(user_input)
+  if !(input_int >= 0 && input_int < 10)
+    turn(board)
+  end
+  move(board, input_int)
+  display_board(board)
+end
 
+ def input_to_index(input)
+    input = input.to_i 
+    input -= 1
+end
 
-#move
-board = [" ", " ", " ", " ", " ", " ", " "]
-
-def move(board_array, user_input, player="X")
- board_array[user_input] = player
-user_input = user_input.to_i
-if (player == "X")
-def move(board_array, user_input, player="X")
- board_array[user_input] = player
-=======
-def move(board, user_input, player="X")
-
-user_input = user_input.to_i
-if (player == "X")
-  board#{user_input} << "X"
-else 
-  board#{user_input} << "Y"
-
-
-
-#valid_move
-def valid_move?(board, index)
-  if position_taken?(board, index) == false && index.between?(0,8)
+ def valid_move?(board, index)
+  if index >= 0 && index < 10  
     return true
-    puts "valid"
-  else 
-    return false
-    puts "invalid"
+  else
+    return false 
   end
 end
 
-#position_taken
-def position_taken?(board, index)
-  
-
-  if (board[index] == " "|| board[index] == "" || board[index] == nil)
-    return false
-  else (!false)
-    return true
-end
-end
-
-
-board = [" "," "," "," "," "," "," "," "," "]
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2] } "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5] } "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8] } "
-end
-display_board(board)
-
-
-
-def turn(user_input)
-  puts "Please enter 1-9:"
+ def move (board, index, char = "X")
+    if valid_move?(board, index) == true
+      board[index] = char
+    end
 end
